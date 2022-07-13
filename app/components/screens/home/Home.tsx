24ca923301dd67cbdec5f12cfcddb7e2d9a4ff3d
IceDiver <admin@example.com>
2022-07-13 11:18:00 +0300

    Add sliders

--- app/components/screens/home/Home.tsx
+++ app/components/screens/home/Home.tsx
@@ -7,8 +7,13 @@ import Meta from '@/utils/meta/Meta';
 import { IHome } from './home.interface';
 import Heading from '@/components/ui/heading/Heading';
 import { toastr } from 'react-redux-toastr';
+import Slider from '@/components/ui/slider/Slider';
+import SubHeading from '@/components/ui/heading/SubHeading';
+import Gallery from '@/components/ui/gallery/Gallery';
 
-const Home: FC<IHome> = () => {
+
+
+const Home: FC<IHome> = ({slides, actors, trendingMovies}) => {
 	return (
 		<Meta
 			title="Watch movies online"
@@ -18,6 +23,15 @@ const Home: FC<IHome> = () => {
 				title="Watch movies online"
 				className="text-gray-300 mb-8 text-xl"
 			/>
+			{slides.length && <Slider slides={slides}/>}
+			<div className='my-10'>
+				<SubHeading title='Trending now'/>
+				{trendingMovies.length && <Gallery items={trendingMovies}/>}
+			</div>
+			<div>
+				<SubHeading title='Best actors'/>
+					{actors.length && <Gallery items={actors}/>}
+			</div>
 		</Meta>
 	);
 };
---
+++ app/components/ui/gallery/GalleryItem.tsx
@@ -0,0 +1,31 @@
+import { FC } from 'react';
+
+import styles from './Gallery.module.scss';
+import { IGalleryItemProps } from './gallery.interface';
+import Link from 'next/link';
+import cn from 'classnames';
+import Image from 'next/image';
+
+const GalleryItem: FC<IGalleryItemProps> = ({item,variant}) => {
+	return <Link href={item.link}>
+		<a className={cn(styles.item, {
+			[styles.withText]: item.content,
+			[styles.horizontal]: variant === 'horizontal',
+			[styles.vertical]: variant === 'vertical',
+		})}>
+			<Image alt={item.name} src={item.posterPath} layout='fill' draggable={false} priority />
+			{item.content && (
+				<div className={styles.content}>
+					<div className={styles.title}>{item.content.title}</div>
+					{
+						item.content.subTitle && (
+							<div className={styles.subTitle}>{item.content.subTitle}</div>
+						)
+					}
+				</div>
+			)}
+		</a>
+	</Link>;
+};
+
+export default GalleryItem;
